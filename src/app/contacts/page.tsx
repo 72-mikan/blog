c687cd5cc88bfb47607b06ea9f317053b21c7c73
root <root@DESKTOP-0UKCNUK.localdomain>
2026-01-16 22:13:35 +0900

    ヘッダーレイアウトの修正

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,9 +1,14 @@
-// import ContactForm from "../components/ContactForm";
-
-// export default function ContactPage() {
-//   return (
-//     <div>
-//       <ContactForm />
-//     </div>
-//   )
-// }
+export default function ContactPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Contact Us</h1>
+      <p className="text-gray-600 mb-4">
+        Get in touch with us. We'd love to hear from you!
+      </p>
+      {/* TODO: Add contact form */}
+      <div className="bg-gray-100 p-6 rounded-lg">
+        <p>Contact form coming soon...</p>
+      </div>
+    </div>
+  );
+}
